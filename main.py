4e089b07a090ecca6a8a8c8f42283e31cd5ab8b7
miris-mimitako <miris.mikitako@gmail.com>
2022-08-27 21:48:28 +0900

    new main

---
+++ main.py
@@ -0,0 +1,15 @@
+import json
+from PIL import Image
+
+class CONVERTER():
+  def __init__(self) -> None:
+    self.target_dir
+
+  
+
+  def __del__(self):
+    pass
+
+if __name__=="__main__":
+  pass
+
