e7e57ece677907f0979cc78dfa4c026e32f2a63b
miris-mimitako <miris.mikitako@gmail.com>
2022-08-27 22:11:52 +0900

    add: main function

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import json
 from PIL import Image
+import pandas as pd
 
 class CONVERTER():
   def __init__(self) -> None:
-    self.target_dir
+    # read settings
+    json_open = open("settings.json")
+    self.df_setting = pd.read_json("settings.json")
+    
+  def convert(self):
+    pass
 
-  
+  def to_jpg(self):
+    pass
 
   def __del__(self):
     pass
 
 if __name__=="__main__":
-  pass
+  con = CONVERTER()
+  con.convert()
+
 
